0f0627628da269ac7c766db8132352fd7969ffd6
Aditya Mishra <adityamishra19880@gmail.com>
2025-04-20 16:56:34 +0530

    major animations done

--- 2project/script.js
+++ 2project/script.js
@@ -4,14 +4,14 @@ function locomotiveAnimation() {
   // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
   const locoScroll = new LocomotiveScroll({
-    el: document.querySelector(".main"),
+    el: document.querySelector("#main"),
     smooth: true,
   });
   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   locoScroll.on("scroll", ScrollTrigger.update);
 
-  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
-  ScrollTrigger.scrollerProxy(".main", {
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
     scrollTop(value) {
       return arguments.length
         ? locoScroll.scrollTo(value, 0, 0)
@@ -26,7 +26,7 @@ function locomotiveAnimation() {
       };
     },
     // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector(".main").style.transform
+    pinType: document.querySelector("#main").style.transform
       ? "transform"
       : "fixed",
   });
@@ -37,51 +37,47 @@ function locomotiveAnimation() {
   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
 }
-
 function loadingAnimation() {
   var tl = gsap.timeline();
-
   tl.from(".line h1", {
     y: 150,
-    stagger: 0.2,
+    stagger: 0.25,
     duration: 0.6,
     delay: 0.5,
   });
-
-  tl.from(".line1-part1", {
+  tl.from("#line1-part1", {
     opacity: 0,
     onStart: function () {
-      var h5timer = document.querySelector(".line1-part1 h5");
+      var h5timer = document.querySelector("#line1-part1 h5");
       var grow = 0;
-      var interval = setInterval(function () {
-        if (grow === 100) {
-          clearInterval(interval);
+      setInterval(function () {
+        if (grow < 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
         }
-        h5timer.innerHTML = grow++;
-      }, 25);
+      }, 27);
     },
   });
-  tl.to(" .line h2", {
-    animationName: "anime",
+  tl.to(".line h2", {
+    animationName: "loaderAnime",
     opacity: 1,
   });
-
-  tl.to(".loader", {
+  tl.to("#loader", {
     opacity: 0,
     duration: 0.2,
     delay: 2.6,
   });
-
-  tl.from(".page1", {
-    delay: 0.2,
+  tl.from("#page1", {
+    delay: 0.1,
     y: 1600,
-    ease: Power4,
     duration: 0.5,
+    ease: Power4,
   });
-  tl.to(".loader", {
+  tl.to("#loader", {
     display: "none",
   });
-  tl.from(".nav", {
+  tl.from("#nav", {
     opacity: 0,
   });
   tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
@@ -89,100 +85,79 @@ function loadingAnimation() {
     stagger: 0.2,
   });
   tl.from(
-    "#hero1 h1, .page2",
+    "#hero1, #page2",
     {
       opacity: 0,
     },
     "-=1.2"
   );
-  tl.from(".before", {
-    opacity: 0,
-    duration: 0.2
-  })
 }
-
 function cursorAnimation() {
-  document.addEventListener("mousemove", function (dets) {
-    gsap.to(".crsr", {
-      left: dets.x,
-      top: dets.y,
-    });
+  Shery.mouseFollower({
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
   });
+  Shery.makeMagnet("#nav-part2 h4");
 
-  Shery.makeMagnet(".nav-part2 h4");
-
-  let videoContainer = document.querySelector(".video-container");
-  let video = document.querySelector(".video-container video");
-  let videoImg = document.querySelector(".video-container img");
-
+  var videoContainer = document.querySelector("#video-container");
+  var video = document.querySelector("#video-container video");
   videoContainer.addEventListener("mouseenter", function () {
     videoContainer.addEventListener("mousemove", function (dets) {
-      gsap.to(".crsr", {
+      gsap.to(".mousefollower", {
         opacity: 0,
       });
-      gsap.to(".video-cursor", {
+      gsap.to("#video-cursor", {
         left: dets.x - 570,
         y: dets.y - 300,
       });
     });
   });
   videoContainer.addEventListener("mouseleave", function () {
-    gsap.to(".crsr", {
+    gsap.to(".mousefollower", {
       opacity: 1,
     });
-    gsap.to(".video-cursor", {
-      top: "-10%",
+    gsap.to("#video-cursor", {
       left: "70%",
+      top: "-15%",
     });
   });
 
   var flag = 0;
-  video.addEventListener("click", function () {
+  videoContainer.addEventListener("click", function () {
     if (flag == 0) {
       video.play();
-      videoImg.style.opacity = 0;
       video.style.opacity = 1;
-
       document.querySelector(
-        ".video-cursor"
+        "#video-cursor"
       ).innerHTML = `<i class="ri-pause-mini-fill"></i>`;
-
-      gsap.to(".video-cursor", {
+      gsap.to("#video-cursor", {
         scale: 0.5,
       });
       flag = 1;
     } else {
       video.pause();
-      videoImg.style.opacity = 1;
       video.style.opacity = 0;
-
       document.querySelector(
-        ".video-cursor"
+        "#video-cursor"
       ).innerHTML = `<i class="ri-play-mini-fill"></i>`;
-
-      gsap.to(".video-cursor", {
+      gsap.to("#video-cursor", {
         scale: 1,
       });
       flag = 0;
     }
   });
 }
-
 function sheryAnimation() {
   Shery.imageEffect(".image-div", {
     style: 5,
+    gooey: true,
+    // debug:true,
     config: {
       a: { value: 2, range: [0, 30] },
       b: { value: 0.75, range: [-1, 1] },
       zindex: { value: -9996999, range: [-9999999, 9999999] },
-      aspect: { value: 0.7833148284490983 },
-      ignoreShapeAspect: { value: true },
-      shapePosition: { value: { x: 0, y: 0 } },
-      shapeScale: { value: { x: 0.5, y: 0.5 } },
-      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
-      shapeRadius: { value: 0, range: [0, 2] },
-      currentScroll: { value: 0 },
-      scrollLerp: { value: 0.07 },
+      aspect: { value: 0.7241195453907675 },
       gooey: { value: true },
       infiniteGooey: { value: false },
       growSize: { value: 4, range: [1, 15] },
@@ -190,22 +165,20 @@ function sheryAnimation() {
       durationIn: { value: 1.5, range: [0.1, 5] },
       displaceAmount: { value: 0.5 },
       masker: { value: true },
-      maskVal: { value: 1.3, range: [1, 5] },
+      maskVal: { value: 1.23, range: [1, 5] },
       scrollType: { value: 0 },
       geoVertex: { range: [1, 64], value: 1 },
       noEffectGooey: { value: true },
-      onMouse: { value: 1 },
-      noise_speed: { value: 0.46, range: [0, 10] },
-      metaball: { value: 0.38, range: [0, 2] },
+      onMouse: { value: 0 },
+      noise_speed: { value: 0.5, range: [0, 10] },
+      metaball: { value: 0.33, range: [0, 2] },
       discard_threshold: { value: 0.5, range: [0, 1] },
-      antialias_threshold: { value: 0, range: [0, 0.1] },
-      noise_height: { value: 0.41, range: [0, 2] },
-      noise_scale: { value: 9.92, range: [0, 100] },
+      antialias_threshold: { value: 0.01, range: [0, 0.1] },
+      noise_height: { value: 0.5, range: [0, 2] },
+      noise_scale: { value: 10, range: [0, 100] },
     },
-    gooey: true,
   });
 }
-
 function flagAnimation() {
   document.addEventListener("mousemove", function (dets) {
     gsap.to("#flag", {
@@ -224,63 +197,55 @@ function flagAnimation() {
     });
   });
 }
-
-function textAnimation() {
+function footerAnimation() {
   var clutter = "";
-  var clutter2 = ""
+  var clutter2 = "";
+  document
+    .querySelector("#footer h1")
+    .textContent.split("")
+    .forEach(function (elem) {
+      clutter += `<span>${elem}</span>`;
+    });
+  document.querySelector("#footer h1").innerHTML = clutter;
+  document
+    .querySelector("#footer h2")
+    .textContent.split("")
+    .forEach(function (elem) {
+      clutter2 += `<span>${elem}</span>`;
+    });
+  document.querySelector("#footer h2").innerHTML = clutter2;
 
-  var text = document.querySelector(".footer-part1 h1")
-  var text2 = document.querySelector(".footer-part1 h2")
-  var textDiv = document.querySelector(".footer-part1")
-
-  text.textContent.split('').forEach((elem) => {
-    clutter += `<span>${elem}</span>`
-  })
-  text.innerHTML = clutter;
-
-  text2.textContent.split("").forEach(elem => {
-    clutter2 +=  `<span>${elem}</span>`
-  });
-  text2.innerHTML = clutter2
-
-
-  textDiv.addEventListener("mouseenter", function(){
-    gsap.to(".footer-part1 h1 span", {
-      opacity: 0,
-      stagger: 0.05
-    })
-    gsap.to(".footer-part1 h2 span", {
-      delay: 0.35,
-      opacity: 1,
-      stagger: 0.01
-    })
-    gsap.to(".footer-part1 svg", {
-      x: 40,
-      delay: 0.05
-    })
-  })
-
-  textDiv.addEventListener("mouseleave", function(){
-    gsap.to(".footer-part1 h1 span", {
-      opacity: 1,
-      stagger: 0.01,
-      delay: 0.35
-    })
-    gsap.to(".footer-part1 h2 span", {
-      opacity: 0,
-      stagger: 0.05
-    })
-    gsap.to(".footer-part1 svg", {
-      x: -40,
-      delay: 0.1
-    })
-  })
-
+  document
+    .querySelector("#footer-text")
+    .addEventListener("mouseenter", function () {
+      gsap.to("#footer h1 span", {
+        opacity: 0,
+        stagger: 0.05,
+      });
+      gsap.to("#footer h2 span", {
+        delay: 0.35,
+        opacity: 1,
+        stagger: 0.1,
+      });
+    });
+  document
+    .querySelector("#footer-text")
+    .addEventListener("mouseleave", function () {
+      gsap.to("#footer h1 span", {
+        opacity: 1,
+        stagger: 0.1,
+        delay: 0.35,
+      });
+      gsap.to("#footer h2 span", {
+        opacity: 0,
+        stagger: 0.05,
+      });
+    });
 }
 
-locomotiveAnimation();
 loadingAnimation();
 cursorAnimation();
+locomotiveAnimation();
 sheryAnimation();
 flagAnimation();
-textAnimation();
+footerAnimation();
